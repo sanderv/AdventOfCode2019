26cac28815a83515752dc3731991fd3e4a107209
Sander Verbruggen <sverbruggen@vxcompany.com>
2019-12-02 20:59:57 +0100

    Day 2 skeleton

---
+++ src/main/kotlin/com/sanderverbruggen/adventofcode/day2/Day2Part1.kt
@@ -0,0 +1,7 @@
+package com.sanderverbruggen.adventofcode.day2
+
+class Day2Part1 {
+    fun runIntcode(intcode: String): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
--- src/test/kotlin/com/sanderverbruggen/adventofcode/day2/Day2Part1Test.kt
+++ src/test/kotlin/com/sanderverbruggen/adventofcode/day2/Day2Part1Test.kt
@@ -1,5 +1,6 @@
 package com.sanderverbruggen.adventofcode.day2
 
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
@@ -13,7 +14,7 @@ class Day2Part1Test {
             "2,4,4,5,99,0        | 2,4,4,5,99,9801",
             "1,1,1,4,99,5,6,0,99 | 30,1,1,4,2,5,6,0,99"
     )
-    internal fun `examples should work`() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    internal fun `examples should work`(intcode: String, expectedResult: String) {
+        Assertions.assertThat(Day2Part1().runIntcode(intcode)).isEqualTo(expectedResult);
     }
 }
