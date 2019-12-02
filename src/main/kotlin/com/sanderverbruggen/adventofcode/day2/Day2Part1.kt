8e13a426bb7fc242b27424d2f976e067951c99a5
Sander Verbruggen <sverbruggen@vxcompany.com>
2019-12-02 21:19:47 +0100

    Day 2 Addition working

--- src/main/kotlin/com/sanderverbruggen/adventofcode/day2/Day2Part1.kt
+++ src/main/kotlin/com/sanderverbruggen/adventofcode/day2/Day2Part1.kt
@@ -2,6 +2,36 @@ package com.sanderverbruggen.adventofcode.day2
 
 class Day2Part1 {
     fun runIntcode(intcode: String): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val program = Program(intcode)
+
+        while (program.getOpcode() != 99) {
+            when (program.getOpcode()) {
+                1 -> program.add()
+            }
+            program.next()
+        }
+
+        return program.toString()
+    }
+}
+
+internal class Program(val program: IntArray) {
+    constructor(program: String) : this(program.split(",").map { it.toInt() }.toIntArray())
+
+    private var pointer = 0
+
+    fun getOpcode() = program[pointer]
+
+    fun add() {
+        val param1 = program[program[pointer + 1]]
+        val param2 = program[program[pointer + 2]]
+        val targetPos = program[pointer + 3]
+        program[targetPos] = param1 + param2
+        println(this)
+    }
+
+    override fun toString() = program.joinToString(",")
+    fun next() {
+        pointer += 4
     }
 }
--- src/test/kotlin/com/sanderverbruggen/adventofcode/day2/Day2Part1Test.kt
+++ src/test/kotlin/com/sanderverbruggen/adventofcode/day2/Day2Part1Test.kt
@@ -1,6 +1,6 @@
 package com.sanderverbruggen.adventofcode.day2
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
@@ -12,9 +12,10 @@ class Day2Part1Test {
             "1,0,0,0,99          | 2,0,0,0,99",
             "2,3,0,3,99          | 2,3,0,6,99",
             "2,4,4,5,99,0        | 2,4,4,5,99,9801",
-            "1,1,1,4,99,5,6,0,99 | 30,1,1,4,2,5,6,0,99"
+            "1,1,1,4,99,5,6,0,99 | 30,1,1,4,2,5,6,0,99",
+            delimiter = '|'
     )
     internal fun `examples should work`(intcode: String, expectedResult: String) {
-        Assertions.assertThat(Day2Part1().runIntcode(intcode)).isEqualTo(expectedResult);
+        assertThat(Day2Part1().runIntcode(intcode)).isEqualTo(expectedResult);
     }
 }
