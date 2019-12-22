5034a46b7f3f2878946c6d1496220efbb0ea83d5
Sander Verbruggen <sander@verbruggen.ws>
2019-12-22 20:55:10 +0100

    Day 12 Part 2 solution. Didn't find this one on my own :(

--- src/main/kotlin/com/sanderverbruggen/adventofcode/day12/Moon.kt
+++ src/main/kotlin/com/sanderverbruggen/adventofcode/day12/Moon.kt
@@ -1,13 +1,14 @@
 package com.sanderverbruggen.adventofcode.day12
 
-import kotlin.math.abs
+import kotlin.math.absoluteValue
 
-class System(moons: List<Moon> = listOf(Moon(0, 0, 0))) {
-    var moons = moons
+class System(val moons: List<Moon> = listOf(Moon(0, 0, 0))) {
+    val moonPairs = moons.permutations()
 
     fun nextStep() {
-        moons.forEach {
-            it.applyGravity(moons)
+        moonPairs.forEach {
+            it.first.applyGravity(it.second)
+            it.second.applyGravity(it.first)
         }
         moons.forEach {
             it.applyVelocity()
@@ -18,7 +19,7 @@ class System(moons: List<Moon> = listOf(Moon(0, 0, 0))) {
             .map { it.totalEnergy() }
             .sum()
 
-    override fun toString() = moons.joinToString("\n")
+    override fun toString() = moonPairs.joinToString("\n")
 }
 
 data class Moon(
@@ -27,15 +28,14 @@ data class Moon(
 ) {
     constructor(x: Int, y: Int, z: Int) : this(Coordinate3D(x, y, z))
 
-    fun applyGravity(otherMoons: List<Moon>) {
-        otherMoons
-                .filter { it != this }
-                .forEach { moon ->
-                    velocity.x += compareValues(moon.position.x, position.x)
-                    velocity.y += compareValues(moon.position.y, position.y)
-                    velocity.z += compareValues(moon.position.z, position.z)
-                }
+    fun applyGravity(otherMoon: Moon) {
+        velocity.x += compareValues(otherMoon.position.x, position.x)
+        velocity.y += compareValues(otherMoon.position.y, position.y)
+        velocity.z += compareValues(otherMoon.position.z, position.z)
     }
+
+    private fun compareValues(a: Int, b: Int) =
+            if (a == b) 0 else if (a < b) -1 else 1
 
     fun applyVelocity() {
         position.x += velocity.x
@@ -47,11 +47,39 @@ data class Moon(
     fun kineticEnergy() = velocity.absSum()
     fun totalEnergy() = potentialEnergy() * kineticEnergy()
 
-    override fun toString() = "pos=$position, vel=$velocity"
+    override fun toString() = "$position|$velocity"
+//    override fun toString() = "pos=" + position + "," + velocity + "$velocity"
 }
 
 class Coordinate3D(var x: Int, var y: Int, var z: Int) {
-    fun absSum() = abs(x) + abs(y) + abs(z)
-    override fun toString() = "<x=%3d, y=%3d, z=%3d>".format(x, y, z)
+    fun absSum() = x.absoluteValue + y.absoluteValue + z.absoluteValue
+    override fun toString() = "$x,$y,$z"
+    //    override fun toString() = "<x=%3d, y=%3d, z=%3d>".format(x, y, z)
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Coordinate3D
+
+        if (x != other.x) return false
+        if (y != other.y) return false
+        if (z != other.z) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = x
+        result = 31 * result + y
+        result = 31 * result + z
+        return result
+    }
+
 }
 
+fun <T> List<T>.permutations(): List<Pair<T, T>> =
+        this.mapIndexed { idx, left ->
+            this.drop(idx + 1).map { right ->
+                Pair(left, right)
+            }
+        }.flatten()
--- src/test/kotlin/com/sanderverbruggen/adventofcode/day12/Day12Test.kt
+++ src/test/kotlin/com/sanderverbruggen/adventofcode/day12/Day12Test.kt
@@ -1,6 +1,8 @@
 package com.sanderverbruggen.adventofcode.day12
 
+import org.apache.commons.math3.util.ArithmeticUtils.lcm
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class Day12Test {
@@ -9,7 +11,7 @@ class Day12Test {
         val ganymede = Moon(Coordinate3D(3, 5, 1), Coordinate3D(0, 0, 0))
         val callisto = Moon(Coordinate3D(5, 3, 1), Coordinate3D(0, 0, 0))
 
-        ganymede.applyGravity(listOf(callisto))
+        ganymede.applyGravity(callisto)
 
         assertThat(ganymede.velocity).isEqualToComparingFieldByField(Coordinate3D(1, -1, 0))
     }
@@ -65,5 +67,86 @@ class Day12Test {
             system.nextStep()
         }
         assertThat(system.totalEnergy()).isEqualTo(6678)
+    }
+
+    @Test
+    internal fun `find repeating step example`() {
+        val system = System(listOf(
+                Moon(-1, 0, 2),
+                Moon(2, -10, -7),
+                Moon(4, -8, 8),
+                Moon(3, 5, -1)
+        ))
+
+        val previousForms: HashSet<String> = hashSetOf()
+
+        var found = false
+        var step = 0
+        while (!found) {
+            system.nextStep()
+            val systemString = system.toString()
+            found = previousForms.contains(systemString)
+            previousForms.add(systemString)
+            step++
+        }
+
+        assertThat(step - 1).isEqualTo(2772)
+    }
+
+    @Test
+    @Disabled
+    internal fun `part 2 solution should be ???`() {
+        val system = System(listOf(
+                Moon(x = -10, y = -10, z = -13),
+                Moon(x = 5, y = 5, z = -9),
+                Moon(x = 3, y = 8, z = -16),
+                Moon(x = 1, y = 3, z = -3)
+        ))
+
+        val previousForms: HashSet<String> = hashSetOf()
+
+        var found = false
+        var step = 0L
+        while (!found) {
+            system.nextStep()
+            val systemString = system.toString()
+            found = previousForms.contains(systemString)
+            previousForms.add(systemString)
+            step++
+            if (step % 1_000_000 == 0L) {
+                println("After $step steps:")
+            }
+        }
+
+        assertThat(step - 1).isEqualTo(0)
+    }
+
+    @Test
+    internal fun `part 2 pirated solution`() {
+        val system = System(listOf(
+                Moon(x = -10, y = -10, z = -13),
+                Moon(x = 5, y = 5, z = -9),
+                Moon(x = 3, y = 8, z = -16),
+                Moon(x = 1, y = 3, z = -3)
+        ))
+        val moons = system.moons
+        val startingX: List<Pair<Int, Int>> = moons.map { it.position.x to it.velocity.x }
+        val startingY: List<Pair<Int, Int>> = moons.map { it.position.y to it.velocity.y }
+        val startingZ: List<Pair<Int, Int>> = moons.map { it.position.z to it.velocity.z }
+        var foundX: Long? = null
+        var foundY: Long? = null
+        var foundZ: Long? = null
+        var stepCount = 0L
+        do {
+            stepCount += 1
+            system.nextStep()
+            foundX = if (foundX == null && startingX == moons.map { it.position.x to it.velocity.x }) stepCount else foundX
+            foundY = if (foundY == null && startingY == moons.map { it.position.y to it.velocity.y }) stepCount else foundY
+            foundZ = if (foundZ == null && startingZ == moons.map { it.position.z to it.velocity.z }) stepCount else foundZ
+
+        } while (foundX == null || foundY == null || foundZ == null)
+        println("$foundX - $foundY - $foundZ --> 496734501382552")
+        val result = lcm(foundX, lcm(foundY, foundZ))
+        assertThat(result).isEqualTo(496734501382552)
     }
 }
