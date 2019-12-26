dd48b982ebba8aa76914aaa242f31fdac594d72e
Sander Verbruggen <sander@verbruggen.ws>
2019-12-26 16:07:05 +0100

    Day 16 100 phases examples working

--- src/test/kotlin/com/sanderverbruggen/adventofcode/day16/FFTTest.kt
+++ src/test/kotlin/com/sanderverbruggen/adventofcode/day16/FFTTest.kt
@@ -2,6 +2,8 @@ package com.sanderverbruggen.adventofcode.day16
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 internal class FFTTest {
     @Test
@@ -13,5 +15,19 @@ internal class FFTTest {
         expectedOutputs.forEach { expectedOutput ->
             assertThat(fft.nextPhase()).isEqualTo(expectedOutput)
         }
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            "80871224585914546619083218645595 | 24176176",
+            "19617804207202209144916044189917 | 73745418",
+            "69317163492948606335995924319873 | 52432133",
+            delimiter = '|'
+    )
+    internal fun `should calculate 100 phases examples`(input: String, expectedResult: String) {
+        val fft = FFT(input)
+        repeat(99) { fft.nextPhase() }
+        val result = fft.nextPhase()
+        assertThat(result).startsWith(expectedResult)
     }
 }
