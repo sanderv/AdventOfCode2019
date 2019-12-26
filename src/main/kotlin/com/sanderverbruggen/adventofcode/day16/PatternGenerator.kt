0ac1e2e185cbd8b5425e3f0f802116fd3c1e8c63
Sander Verbruggen <sander@verbruggen.ws>
2019-12-26 15:39:50 +0100

    Day 16 Pattern Generator improved

--- src/main/kotlin/com/sanderverbruggen/adventofcode/day16/PatternGenerator.kt
+++ src/main/kotlin/com/sanderverbruggen/adventofcode/day16/PatternGenerator.kt
@@ -5,12 +5,11 @@ class PatternGenerator(private val multiply: Int) {
         val basePattern = arrayListOf(0, 1, 0, -1)
     }
 
-    val pattern: List<Int> = basePattern.flatMap { List(multiply) { _ -> it } }
     var position = if (multiply == 1) 0 else 1
 
     fun next(): Int {
-        val result = pattern[position]
-        position = ++position % pattern.size
+        val result = basePattern[position / multiply]
+        position = ++position % (basePattern.size * multiply)
         return result;
     }
 }
