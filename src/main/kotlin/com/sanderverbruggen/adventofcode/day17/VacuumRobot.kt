39ef4b7ca6425e1cc167166cbad761037f936053
Sander Verbruggen <sander@verbruggen.ws>
2019-12-26 21:18:04 +0100

    Day 17 Part 2 solution

--- src/main/kotlin/com/sanderverbruggen/adventofcode/day17/VacuumRobot.kt
+++ src/main/kotlin/com/sanderverbruggen/adventofcode/day17/VacuumRobot.kt
@@ -16,21 +16,61 @@ class VacuumRobot(input: String) : Robot<VacuumRobotCellType>(input) {
                 val ascii = brain.outputChannel.receive()
                 val type = VacuumRobotCellType.fromAscii(ascii)
                 if (type != null) {
-                    markCell(Point(x, y), type)
+                    val location = Point(x, y)
+                    markCell(location, type)
+                    if (type.isDroid()) {
+                        currentLocation = location
+                    }
                 }
                 if (ascii == 10L) {
-                    println()
                     x = 0
                     y++
                 } else {
-                    print(ascii.toChar())
                     x++
                 }
             }
         }
+        printArea()
         return findCrossings()
                 .map { it.x * it.y }
                 .sum()
+    }
+
+    fun solvePart2(): Int {
+        var result = -1
+        runBlocking {
+            val job = launch { brain.suspendedRun() }
+            launch {
+                // Drain output
+                while (!brain.outputChannel.isClosedForReceive) {
+                    val ascii = brain.outputChannel.receive()
+                    if (ascii < 255) {
+                        if (ascii == 10L)
+                            println()
+                        else
+                            print(ascii.toChar())
+                    } else {
+                        result = ascii.toInt()
+                    }
+                }
+            }
+            val instructions = listOf(
+                    "A,B,A,C,B,C,A,B,A,C",
+                    "R,10,L,8,R,10,R,4",
+                    "L,6,L,6,R,10",
+                    "L,6,R,12,R,12,R,10",
+                    "n"
+            )
+
+            instructions.forEach { moveFunction ->
+                moveFunction.forEach { instruction ->
+                    val instr = instruction.toLong()
+                    brain.inputChannel.send(instr)
+                }
+                brain.inputChannel.send(10)
+            }
+        }
+        return result
     }
 
     private fun markCell(point: Point, type: VacuumRobotCellType) {
@@ -57,11 +97,18 @@ enum class VacuumRobotCellType(
 ) {
     SCAFFOLD('#'),
     FREE('.'),
-    CAMERA('O');
+    CAMERA('O'),
+    DROID_LEFT('<'),
+    DROID_RIGHT('>'),
+    DROID_UP('^'),
+    DROID_DOWN('v'),
+    ;
 
     companion object {
         fun fromAscii(ascii: Long): VacuumRobotCellType? = values().firstOrNull { it.display.toInt() == ascii.toInt() }
     }
+
+    fun isDroid() = name.startsWith("DROID")
 
     override fun toString() = display.toString()
 }
--- src/test/kotlin/com/sanderverbruggen/adventofcode/day17/Day17Test.kt
+++ src/test/kotlin/com/sanderverbruggen/adventofcode/day17/Day17Test.kt
@@ -9,4 +9,9 @@ class Day17Test {
     internal fun `part 1 solution should be 7280`() {
         assertThat(VacuumRobot(readFile("day17/input.txt")).solvePart1()).isEqualTo(7280)
     }
+
+    @Test
+    internal fun `part 2 solution should be 1045393`() {
+        assertThat(VacuumRobot(readFile("day17/input.txt").replaceRange(0, 1, "2")).solvePart2()).isEqualTo(1045393)
+    }
 }
